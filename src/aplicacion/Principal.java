fe7c0be6a745c5c9576fc5ce51bd77b7d98ff8e4
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 16:51:42 +0100

    ejercicio10 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,12 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.Ejercicio10;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Principal {
+    public static void main (String[]args){
+        ArrayList lista= new ArrayList();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(" "+Ejercicio10.listaPar(lista, n));
+    }
 }
--- src/dominio/Ejercicio10.java
+++ src/dominio/Ejercicio10.java
@@ -1,2 +1,17 @@
-package dominio;public class Ejercicio10 {
+package dominio;
+import java.util.ArrayList;
+public class Ejercicio10 {
+    public static ArrayList listaPar(ArrayList lista,int n){
+        if(n==2){
+            lista.add(n);
+            return lista;
+        }else
+        if(n%2==0){
+            lista.add(n);
+            return listaPar(lista,n-1);
+        }else{
+            return listaPar(lista,n-1);
+        }
+
+    }
 }
